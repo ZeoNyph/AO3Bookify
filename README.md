d0c09e39ecf9e72d364952d137deccd7bfe0d8e7
Mithun Sivanesan <mithunsivanesan@gmail.com>
2025-12-18 11:13:05 +0400

    add black for formatting, add support for AO3 URLs, fix note detection, add func to remove unneccessary <p> tags

--- README.md
+++ README.md
@@ -7,20 +7,21 @@ A Python script that converts an HTML download of an AO3 fanfiction into a reade
 
 ## Usage
 
-1. Download the fanfiction you wish to bookify from AO3 (either via clicking Download at the top left corner, then HTML, or by just right clicking the fic when viewing the whole work and selecting Save Page As)
+1. Download the fanfiction you wish to bookify from AO3 (either via clicking Download at the top left corner, then HTML, or by just right clicking the fic when viewing the whole work and selecting Save Page As).
 ![alt text](img/image.png)
+  - Alternatively, you can also pass a URL to the fic and AO3Bookify will download its contents automatically.
 2. Run AO3Bookify by running 
-```console
-$ uv run parser.py
-```
+  ```console
+  $ uv run parser.py
+  ```
 Usage instructions are down below.
 ```
-usage: AO3Bookify [-h] [-o OUTPUT] [--no-notes] file
+usage: AO3Bookify [-h] [-o OUTPUT] [--no-notes] path
 
 Python program that parses a HTML download of an AO3 fic.
 
 positional arguments:
-  file
+  path                 The path to the HTML file containing the fic. Can also be a URL to the fic itself.
 
 options:
   -h, --help           show this help message and exit
--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from argparse import ArgumentParser, Namespace
 from weasyprint import HTML, CSS
-import os, platform
+from urllib.parse import urlparse
+import os, platform, requests
 
-parser : ArgumentParser
-args : Namespace
+parser: ArgumentParser
+args: Namespace
 
-pdf_stylesheet : str = """
+pdf_stylesheet: str = """
 @page {
   size: 110mm 170mm;
 }
@@ -30,7 +31,10 @@ pdf_stylesheet : str = """
 }
 
 img {
-  display: none;
+  display: block;
+  object-fit: contain;
+  width: 100%;
+  height: auto;
 }
 
 html {
@@ -86,29 +90,39 @@ br::after {
 
 ## Filter functions
 
-def is_note(css_class) -> bool:
-    return css_class is not None and ("notes" in css_class or "meta group" in css_class or "meta" in css_class)
+
+def is_note(tag: Tag) -> bool:
+    return (
+        tag.has_attr("class")
+        and tag.has_attr("id")
+        and ("notes" in tag["class"] or "meta" in tag["class"] or "end" in tag["class"])
+    )
+
 
 def is_heading(css_class) -> bool:
     return css_class is not None and ("heading" in css_class or "title" in css_class)
 
+
 ## Helper functions
 
+
 def remove_author_notes(contents: BeautifulSoup) -> BeautifulSoup:
-    notes = contents.find_all('div', class_=is_note)
+    notes = contents.find_all("div", class_=is_note)
     for note in notes:
-        head = note.find("h2", class_='heading')
-        
+        head = note.find("h2", class_="heading")
+
         if head:
             note.insert_before(head)
         note.decompose()
     return contents
 
+
 def remove_chapter_text_headings(contents: BeautifulSoup) -> BeautifulSoup:
-    chp_text_headings = contents.find_all('h3', string="Chapter Text")
+    chp_text_headings = contents.find_all("h3", string="Chapter Text")
     for hd in chp_text_headings:
         hd.decompose()
     return contents
+
 
 def format_headings(contents: BeautifulSoup) -> BeautifulSoup:
     headings = contents.find_all(class_=is_heading)
@@ -116,8 +130,17 @@ def format_headings(contents: BeautifulSoup) -> BeautifulSoup:
         a_text = heading.a.extract().get_text() if heading.a else ""
         heading_text = heading.get_text()
         heading.string = a_text + heading_text.strip()
-        heading.name = 'h2'
+        heading.name = "strong" if heading.find_parent(is_note) else "h2"
     return contents
+
+
+def remove_whitespace_paragraphs(contents: BeautifulSoup) -> BeautifulSoup:
+    ws = contents.find_all("p")
+    for para in ws:
+        if para.get_text(strip=True) == "" and len(para.find_all(recursive=False)) == 0:
+            para.decompose()
+    return contents
+
 
 def write_to_pdf(input: BeautifulSoup, filepath: str):
     if not filepath.endswith(".pdf"):
@@ -126,45 +149,89 @@ def write_to_pdf(input: BeautifulSoup, filepath: str):
     HTML(string=str(input)).write_pdf(target=filepath, stylesheets=[css])
     print(f"File saved at: {os.path.abspath(filepath)}")
 
+
 def get_fic_metadata(contents: BeautifulSoup) -> dict:
-    
-    title = contents.find("div", class_="meta").h1.get_text().strip() if contents.find("div", class_="meta") else None
+    title = (
+        contents.find("div", class_="meta").h1.get_text().strip()
+        if contents.find("div", class_="meta")
+        else None
+    )
     if not title:
-      title = contents.find("div", class_={"preface", "group"}).h2.get_text().strip()
+        title = contents.find("div", class_={"preface", "group"}).h2.get_text().strip()
     author = contents.find("a", rel="author").get_text().strip()
-    return {
-        "title": title,
-        "author": author
-    }
+    return {"title": title, "author": author}
+
 
 def init_parser():
     global parser
     parser = ArgumentParser(
         prog="AO3Bookify",
-        description="Python program that parses a HTML download of an AO3 fic."
+        description="Python program that parses a HTML download of an AO3 fic.",
     )
-    parser.add_argument('file')
-    parser.add_argument('-o', '--output')
-    parser.add_argument('--no-notes', action='store_true', default=False, help="Remove author notes from the output.")
+    parser.add_argument(
+        "path",
+        help="The path to the HTML file containing the fic. Can also be a URL to the fic itself.",
+    )
+    parser.add_argument("-o", "--output")
+    parser.add_argument(
+        "--no-notes",
+        action="store_true",
+        default=False,
+        help="Remove author notes from the output.",
+    )
     return parser
+
+
+def get_from_url(url: str) -> str:
+    parsed_url = urlparse(url)
+    relative_path = parsed_url.path
+    if "chapters" in relative_path:
+        parts = relative_path.split("/")
+        relative_path = "/".join(parts[:3])
+        parsed_url = parsed_url._replace(path=relative_path)
+    if not parsed_url.query.startswith("view_full_work"):
+        parsed_url = parsed_url._replace(query="view_full_work=true")
+    return requests.get(parsed_url.geturl()).text
+
+
+def get_from_file(filepath: str) -> str:
+    with open(os.path.abspath(filepath), "rt") as fic:
+        return fic.read()
+
+
+def parse_fic(path: str) -> BeautifulSoup:
+    content = ""
+    if path.startswith("https://archiveofourown.org"):
+        print(
+            "Requesting fic data from AO3...\n[Depending on the fic, this may take a while.]"
+        )
+        content = get_from_url(path)
+    elif path.endswith(".html"):
+        content = get_from_file(path)
+    else:
+        print("Path not supported")
+    return BeautifulSoup(content, "lxml")
+
 
 ## Main loop
 
 if __name__ == "__main__":
-    init_parser()
-    args = parser.parse_args()
+    try:
+        init_parser()
+        args = parser.parse_args()
 
-    with open(os.path.abspath(args.file), 'rt') as fic:
-        parsed_html = BeautifulSoup(fic, "lxml")
+        parsed_html = parse_fic(args.path)
         data = get_fic_metadata(parsed_html)
         print(f"Bookifying {data['title']} by {data['author']}")
-        contents = parsed_html.find('div', id="chapters")
+        contents = parsed_html.find("div", id="chapters")
         if args.no_notes:
             contents = remove_author_notes(contents)
         contents = remove_chapter_text_headings(contents)
         contents = format_headings(contents)
+        contents = remove_whitespace_paragraphs(contents)
 
-    filepath = f"{data["title"]}.pdf" if args.output is None else args.output
-    write_to_pdf(contents, filepath=filepath)
-        
-    
+        filepath = f"{data["title"]}.pdf" if args.output is None else args.output
+        write_to_pdf(contents, filepath=filepath)
+    except KeyboardInterrupt:
+        print("\nExiting AO3Bookify...")
+        exit(1)
